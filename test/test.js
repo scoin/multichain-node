cd2182d6707ee2425fa2eeb7ee3ab11b40d8d970
Greg Piccolo <greg.piccolo@gmail.com>
2016-09-20 19:16:00 -0400

    test alpha 24 api

--- test/test.js
+++ test/test.js
@@ -13,6 +13,7 @@ const bluebird = require("bluebird");
 const multichain = bluebird.promisifyAll(require("../index.js")(connection), {suffix: "Promise"});
 
 let listenForConfirmations = (txid, cb) => {
+    console.log("WAITING FOR CONFIRMATIONS")
     var interval = setInterval(() => {
         getConfirmations(txid, (err, confirmations) => {
             if(confirmations > 0){
@@ -38,43 +39,58 @@ let getConfirmations = (txid, cb) => {
 
 let startTests = () => {
     console.log("Running Tests")
+
+    console.log("TEST: GET INFO")
     multichain.getInfo((err, res) => {
         assert(res, "Could not get info");
     })
 
+    console.log("TEST: GET MINING INFO")
     multichain.getMiningInfo((err, res) => {
         assert(res, "Could not get mining info");
     })
 
+    console.log("TEST: GET BLOCKCHAIN PARAMS")
     multichain.getBlockchainParams((err, res) => {
         assert(res);
     })
 
+    console.log("TEST: GET PEER INFO")
     multichain.getPeerInfo((err, res) => {
         assert(res, "Could not get peer info");
     })
 
+    console.log("TEST: GET BLOCK HASH")
     multichain.getBlockHash({height: 1}, (err, blockHash) => {
         assert(blockHash, "Could not get block hash at height 1");
+        
+        console.log("TEST: GET BLOCK")
         multichain.getBlock({hash: blockHash}, (err, block) => {
             assert(block, "Could not get block from hash")
         })
     })
 
+    console.log("TEST: GET NEW ADDRESS")
     multichain.getNewAddressPromise()
     .then(address => {
         assert(address, "Could not get new address")
         this.address1 = address;
+
+        console.log("TEST: VALIDATE ADDRESS")
         return multichain.validateAddressPromise({address: this.address1})
     })
     .then(addrInfo => {
         assert(addrInfo);
         assert(addrInfo.isvalid === true);
         assert(addrInfo.address === this.address1);
+
+        console.log("TEST: DUMP PRIVATE KEY")
         return multichain.dumpPrivKeyPromise({address: this.address1})
     })
     .then(privateKey => {
         assert(privateKey)
+
+        console.log("TEST: GRANT")
         return multichain.grantPromise({
             addresses: this.address1,
             permissions: "send,receive,issue,admin"
@@ -82,11 +98,15 @@ let startTests = () => {
     })
     .then(permissionsTxid => {
         assert(permissionsTxid)
+        
+        console.log("TEST: GET NEW ADDRESS")
         return multichain.getNewAddressPromise();
     })
     .then(address2 => {
         assert(address2, "Could not get new address 2");
         this.address2 = address2;
+
+        console.log("TEST: GRANT FROM")
         return multichain.grantFromPromise({
             from: this.address1,
             to: this.address2,
@@ -95,19 +115,27 @@ let startTests = () => {
     })
     .then(permissionsTxid => {
         assert(permissionsTxid)
+        
+        console.log("TEST: GRANT WITH METADATA")
         return multichain.grantWithMetadataPromise({addresses: this.address2, permissions: "send", data: new Buffer("some important data").toString("hex")})
     })
     .then(permissionsTxid => {
         assert(permissionsTxid)
+        
+        console.log("TEST: GRANT WITH METADATA FROM")
         return multichain.grantWithMetadataFromPromise({from: this.address1, to: this.address2, permissions: "receive", data: new Buffer("another important data").toString("hex")})
     })
     .then(permissionsTxid => {
         assert(permissionsTxid)
+        
+        console.log("TEST: GET WALLET TRANSACTION")
         return multichain.getWalletTransactionPromise({txid: permissionsTxid})
     })
     .then(txData => {
         let msg = new Buffer(txData.data[0], "hex").toString("utf8");
         assert.equal(msg, "another important data")
+        
+        console.log("TEST: LIST PERMISSIONS")
         return multichain.listPermissionsPromise({
             addresses: `${this.address1},${this.address2}`,
             verbose: true
@@ -133,10 +161,14 @@ let startTests = () => {
         assert("receive" in permissionSet[this.address2].types)
         assert("issue" in permissionSet[this.address2].types)
         assert(permissionSet[this.address2].admin === this.address1)
+        
+        console.log("TEST: REVOKE")
         return multichain.revokePromise({addresses: this.address2, permissions: "issue"})
     })
     .then(revokeTxid => {
         assert(revokeTxid);
+        
+        console.log("TEST: ISSUE")
         return multichain.issuePromise({
             address: this.address1, 
             asset: {
@@ -167,10 +199,14 @@ let startTests = () => {
 let confirmCallback1 = () => {
     bluebird.bind(this)
     .then(() => {
+        
+        console.log("TEST: LIST ASSETS")
         return multichain.listAssetsPromise()
     })
     .then(assets => {
         assert(assets)
+        
+        console.log("TEST: ISSUE FROM")
         return multichain.issueFromPromise({
             from: this.address1,
             to: this.address2,
@@ -184,6 +220,8 @@ let confirmCallback1 = () => {
     .then(issueTxid => {
         assert(issueTxid);
         this.issueTxid = issueTxid;
+        
+        console.log("TEST: GET ADDRESS BALANCES")
         return multichain.getAddressBalancesPromise({
             address: this.address2,
             minconf: 0
@@ -209,11 +247,15 @@ let confirmCallback1 = () => {
 let confirmCallback2 = () => {
     bluebird.bind(this)
     .then(() => {
+        
+        console.log("TEST: GET TOTAL BALANCES")
         return multichain.getTotalBalancesPromise({minconf: 1})
     })
     .then(totalBalances => {
         assert(totalBalances)
         assert(totalBalances.length === 2)
+        
+        console.log("TEST: SEND ASSET FROM")
         return multichain.sendAssetFromPromise({
             from: this.address1,
             to: this.address2,
@@ -223,6 +265,8 @@ let confirmCallback2 = () => {
     })
     .then(txid => {
         assert(txid);
+        
+        console.log("TEST: SEND FROM ADDRESS")
         return multichain.sendFromAddressPromise({
             from: this.address2,
             to: this.address1,
@@ -233,12 +277,16 @@ let confirmCallback2 = () => {
     })
     .then(txid => {
         assert(txid);
+
+        console.log("TEST: GET WALLET TRANSACTION")
         return multichain.getWalletTransactionPromise({
             txid: txid
         })
     })
     .then(tx => {
         assert(tx);
+
+        console.log("TEST: SEND WITH METADATA FROM")
         return multichain.sendWithMetadataFromPromise({
             from: this.address1,
             to: this.address2,
@@ -269,12 +317,14 @@ let confirmCallback2 = () => {
 let confirmCallback3 = () => {
     bluebird.bind(this)
     .then(() => {
+        console.log("TEST: GET RAW TRANSACTION")
         return multichain.getRawTransactionPromise({
             txid: this.txid
         })
     })
     .then(txHex => {
         assert(txHex);
+        console.log("TEST: DECODE RAW TRANSACTION")
         return multichain.decodeRawTransactionPromise({
             hexstring: txHex
         })
@@ -282,12 +332,16 @@ let confirmCallback3 = () => {
     .then(tx => {
         assert(tx);
         assert(tx.data[0] = new Buffer("a nice message, for you").toString("hex"));
+        
+        console.log("TEST: LIST ADDRESS TRANSACTIONS")
         return multichain.listAddressTransactionsPromise({
             address: this.address2
         })
     })
     .then(transactions => {
         assert(transactions)
+
+        console.log("TEST: PREPARE LOCK UNSPENT FROM")
         return bluebird.join(multichain.prepareLockUnspentFromPromise({
             from: this.address1,
             assets: {
@@ -314,10 +368,14 @@ let confirmCallback3 = () => {
         rawTxData.amounts[this.address2] = {
             foocoin: 100
         }
+
+        console.log("TEST: CREATE RAW TRANSACTION")
         return multichain.createRawTransactionPromise(rawTxData)
     })
     .then(rawTx => {
         assert(rawTx);
+
+        console.log("TEST: APPEND RAW METADATA")
         return multichain.appendRawMetadataPromise({
             tx: rawTx,
             data: new Buffer("some more metadata").toString("hex")
@@ -325,6 +383,8 @@ let confirmCallback3 = () => {
     })
     .then(rawTx => {
         assert(rawTx);
+
+        console.log("TEST: SIGN RAW TRANSACTION")
         return multichain.signRawTransactionPromise({
             hexstring: rawTx
         })
@@ -332,12 +392,16 @@ let confirmCallback3 = () => {
     .then(signedTx => {
         assert(signedTx);
         assert(signedTx.hex);
+
+        console.log("TEST: SEND RAW TRANSACTION")
         return multichain.sendRawTransactionPromise({
             hexstring: signedTx.hex
         })
     })
     .then(txid => {
         assert(txid);
+
+        console.log("TEST: GET RAW TRANSACTION")
         return multichain.getRawTransactionPromise({
             txid: txid,
             verbose: 1
@@ -345,6 +409,8 @@ let confirmCallback3 = () => {
     })
     .then(tx => {
         assert(tx);
+
+        console.log("TEST: GET TX OUT")
         return multichain.getTxOutPromise({
             txid: tx.txid,
             vout: tx.vout[0].n,
@@ -354,10 +420,14 @@ let confirmCallback3 = () => {
     .then(txOut => {
         assert(txOut);
         assert(txOut.assets.length === 1);
+
+        console.log("TEST: LIST LOCK UNSPENT")
         return multichain.listLockUnspentPromise()
     })
     .then(unspent => {
         assert(unspent);
+
+        console.log("TEST: LOCK UNSPENT")
         return multichain.lockUnspentPromise({
             unlock: true,
             outputs: unspent
@@ -365,6 +435,8 @@ let confirmCallback3 = () => {
     })
     .then(unlocked => {
         assert(unlocked);
+
+        console.log("TEST: GET ADDRESS BALANCES")
         return multichain.getAddressBalancesPromise({
             minconf: 0,
             address: this.address1
@@ -372,6 +444,7 @@ let confirmCallback3 = () => {
     })
     .then(balances => {
         assert(balances);
+        console.log("TEST: PREPARE LOCK UNSPENT")
         return multichain.prepareLockUnspentPromise({
             assets: {
                 barcoin: 500
@@ -381,6 +454,8 @@ let confirmCallback3 = () => {
     .then(outputs => {
         assert(outputs);
         this.outputForExchange = outputs;
+
+        console.log("TEST: PREPARE LOCK UNSPENT")
         return multichain.prepareLockUnspentPromise({
             assets: {
                 foocoin: 200
@@ -388,6 +463,9 @@ let confirmCallback3 = () => {
         })
     })
     .then(outputs => {
+        assert(outputs)
+
+        console.log("TEST: CREATE RAW EXCHANGE")
         return multichain.createRawExchangePromise({
             txid: outputs.txid,
             vout: outputs.vout,
@@ -398,6 +476,8 @@ let confirmCallback3 = () => {
     })
     .then(rawPartialTx => {
         assert(rawPartialTx);
+
+        console.log("TEST: APPEND RAW EXCHANGE")
         return multichain.appendRawExchangePromise({
             hexstring: rawPartialTx,
             txid: this.outputForExchange.txid,
@@ -410,18 +490,24 @@ let confirmCallback3 = () => {
     .then(rawExchange => {
         assert(rawExchange);
         this.rawExchange = rawExchange;
+
+        console.log("TEST: DECODE RAW EXCHANGE")
         return multichain.decodeRawExchangePromise({
             hexstring: rawExchange.hex
         })
     })
     .then(decodedExchange => {
         assert(decodedExchange);
+
+        console.log("TEST: SEND RAW TRANSACTION")
         return multichain.sendRawTransactionPromise({
             hexstring: this.rawExchange.hex
         })
     })
     .then(txid => {
         assert(txid);
+
+        console.log("TEST: GET WALLET TRANSACTION")
         return multichain.getWalletTransactionPromise({
             txid: txid,
             verbose: true
@@ -429,6 +515,8 @@ let confirmCallback3 = () => {
     })
     .then(tx => {
         assert(tx);
+
+        console.log("TEST: ISSUE MORE")
         return multichain.issueMorePromise({
             address: this.address2,
             asset: "foocoin",
@@ -454,6 +542,7 @@ let confirmCallback3 = () => {
 let confirmCallback4 = () => {
     bluebird.bind(this)
     .then(() => {
+        console.log("TEST: GET MULTI BALANCES")
         return multichain.getMultiBalancesPromise({
             addresses: [this.address1, this.address2],
             assets: ["foocoin", "barcoin"]
@@ -461,6 +550,8 @@ let confirmCallback4 = () => {
     })
     .then(balances => {
         assert(balances)
+
+        console.log("TEST: CREATE MULTI SIG")
         return multichain.createMultiSigPromise({
             nrequired: 2,
             keys: [this.address1, this.address2]
@@ -469,22 +560,148 @@ let confirmCallback4 = () => {
     .then(multiSigWallet => {
         assert(multiSigWallet);
         assert(multiSigWallet.address);
+
+        console.log("TEST: CREATE STREAM")
+        return multichain.createPromise({
+            name: "stream1",
+            open: true
+        })
     })
-    .then(() => {
+    .then(stream => {
+        assert(stream)
+
+        console.log("TEST: CREATE STREAM FROM")
+        return multichain.createPromise({
+            name: "stream2",
+            open: true,
+            from: this.address2,
+            details: {
+                "something": "yes"
+            }
+        })
+    })
+    .then(stream2 => {
+        assert(stream2)
+
+        console.log("TEST: LIST STREAMS")
+        return multichain.listStreamsPromise()
+    })
+    .then(streamList => {
+        assert.equal(streamList.length, 3)
+
+        console.log("TEST: SUBSCRIBE STREAM")
+        return multichain.subscribePromise({
+            stream: "stream1"
+        })
+    })
+    .then(subscribed => {
+        console.log("TEST: PUBLISH STREAM")
+        return multichain.publishPromise({
+            stream: "stream1",
+            key: "test1",
+            data: new Buffer("some stream data").toString("hex")
+        })
+    })
+    .then(hexstring => {
+        assert(hexstring)
+
+        console.log("TEST: PUBLISH FROM")
+        return multichain.publishFromPromise({
+            from: this.address2,
+            stream: "stream1",
+            key: "test2",
+            data: new Buffer("some more stream data").toString("hex")
+        })
+    })
+    .then(hexstring => {
+        assert(hexstring);
+
+        console.log("TEST: LIST STREAM KEYS");
+        return multichain.listStreamKeysPromise({
+            stream: "stream1"
+        })
+    })
+    .then(streamKeys => {
+        assert.equal(streamKeys.length, 2);
+
+        console.log("TEST: LIST STREAM KEY ITEMS");
+        return multichain.listStreamKeyItemsPromise({
+            stream: "stream1",
+            key: "test1",
+            verbose: true
+        })
+    })
+    .then(streamKeyItems => {
+        assert.equal(streamKeyItems.length, 1);
+        assert.equal(streamKeyItems[0].key, "test1");
+        this.streamData = streamKeyItems[0].data;
+
+        console.log("TEST: GET TXOUTDATA")
+        return multichain.getTxOutDataPromise({
+            txid: streamKeyItems[0].txid,
+            vout: 0
+        })
+
+    })
+    .then(txData => {
+        assert(txData)
+        assert.equal(txData, this.streamData)
+
+        console.log("TEST: LIST STREAM ITEMS")
+        return multichain.listStreamItemsPromise({
+            stream: "stream1",
+            verbose: true
+        })
+    })
+    .then(streamItems => {
+        assert(streamItems)
+        assert.equal(streamItems.length, 2)
+
+        console.log("TEST: LIST STREAM PUBLISHER ITEMS")
+        return multichain.listStreamPublisherItemsPromise({
+            stream: "stream1",
+            address: this.address2,
+            verbose: true
+        })
+    })
+    .then(streamPublisherItems => {
+        assert(streamPublisherItems)
+
+        console.log("TEST: LIST STREAM PUBLISHERS")
+        return multichain.listStreamPublishersPromise({
+            stream: "stream1"
+        })
+    })
+    .then(streamPublishers => {
+        assert(streamPublishers)
+
+        console.log("TEST: UNSUBSCRIBE")
+        return multichain.unsubscribePromise({
+            stream: "stream1"
+        })
+    })
+    .then(unsubbed => {
+
+        console.log("TEST: PAUSE")
         return multichain.pausePromise({
             tasks: "mining,incoming"
         })
     })
     .then(() => {
+
+        console.log("TEST: SET LAST BLOCK")
         return multichain.setLastBlockPromise({
             hash: "1"
         })
     })
     .then(hash => {
         assert(hash);
+
+        console.log("TEST: CLEAR MEMPOOL")
         return multichain.clearMemPoolPromise()
     })
     .then(() => {
+        console.log("TEST: RESUME")
         return multichain.resumePromise({
             tasks: "mining,incoming"
         })
